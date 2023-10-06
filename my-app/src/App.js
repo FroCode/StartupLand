b104d7659c143d1e37dda2e0eb591d57f3b2fa16
Haddouche Maamar <frocodeof@gmail.com>
2023-10-06 23:21:56 +0200

    de

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This From Maamar <code>src/App.js</code> and save to reload.
+          This From Just <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link text-sky-400 "
