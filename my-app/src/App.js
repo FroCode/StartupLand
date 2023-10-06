3b026e54c521ed03f478f0068f66be8fbcae7944
Haddouche Maamar <frocodeof@gmail.com>
2023-10-06 23:21:34 +0200

    Trying text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This From FroCode <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-sky-400 "
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
