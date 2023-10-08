613fcc9e7cfa68ba0e004058affc89403a1bad4f
Haddouche Maamar <frocodeof@gmail.com>
2023-10-08 22:51:14 +0200

    Finish NavBar

--- my-app/src/Components/NavBar.jsx
+++ my-app/src/Components/NavBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { logo } from '../Assets/icons'
+import { lock, logo } from '../Assets/icons'
 function NavBar() {
     return (
         <div className=''>
             <div className="NavBar flex text-white p-5"> 
                 
                 <div className="logo flex ml-20">{logo} <h2 className='mt-1 ml-5 text-[20px]	font-bold'>StartupLand</h2></div>
-                <div className="list-item shrink w- h-1 ml-auto">
-                <ul className='flex m-2 list-none '>
-                    <li className='mr-4 font-semibold list-none '>HOME  </li>
-                    <li className='mr-4 font-semibold'>ADVERSITES </li>
-                    <li className='mr-4 font-semibold'>SUPPORT</li>
+                <div className="shrink w- h-1 ml-auto">
+                <ul className='flex m-2 list-none'>
+                    <li className='mr-4  list-none '>Home  </li>
+                    <li className='mr-4 '>Adversite </li>
+                    <li className='mr-4 '>Supports</li>
+                    <li className='mr-4 '>About</li>
                 </ul>
                 </div>
-                <div className="left-login ml-auto">
-                <a className='m-2 font-bold'>See Pricing</a>
-                <a className='m-2  bg-white p-3 text-[#87A9FF] font-bold'>Join Community</a>
+                <div className="left-login flex ml-auto">
+                <a className='m-2 font-bold flex p-2 text-base	'> <i className='mr-3 '>{lock}</i> Login</a>
+                <a className='m-2  bg-white p-3 text-[#5B2B9D] rounded font-bold'>Join Community</a>
                 </div>
             </div>
         </div>
