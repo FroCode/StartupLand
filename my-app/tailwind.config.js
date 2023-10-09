ce09b64021b0f5fb95a56b34eb23410cf1ecfb64
Haddouche Maamar <frocodeof@gmail.com>
2023-10-09 10:21:58 +0200

    Update

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jsx,svg,js}"],
+  content: ["./src/**/*.{html,jsx,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '800px',
+      xl: '1200px',
+    },
   },
   plugins: [],
+  
 }
-
