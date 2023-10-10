8d8114a605c9401c1128c9208a81b3950b6e7e16
Haddouche Maamar <frocodeof@gmail.com>
2023-10-10 13:53:46 +0200

    Up

--- my-app/src/Components/SingleCard.jsx
+++ my-app/src/Components/SingleCard.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const PricingCard = ({ title, price, features, par, key , num }) => {
 
   return (
-    <div className={`bg-white rounded-lg  p-20 mx-4 hover:border hover:shadow-lg  ${num === 1  ?  " border shadow-lg mb-20" : ""} `}>
-      <h2 className="text-2xl font-bold mb-10">{title}</h2>
+    <div className={`bg-white rounded-lg ml-2 p-10 mx-4 hover:border hover:shadow-xl  ${num === 1  ?  "  shadow-lg  " : ""} `}>
+      <h2 className="text-2xl font-bold mb-5">{title}</h2>
       <p className='pb-5'>{par}</p>
-      <p className="text-3xl font-bold  mb-10">${price}</p>
+      <p className="text-2xl font-bold  mb-10">${price}</p>
       <ul className="text-gray-600">
-      <a className='m-2  bg-[#5B2B9D] p-3  text-white  rounded font-black'>Join Community</a> 
+      <a className='m-2  bg-[#5B2B9D] p-4  text-white  rounded font-black'>Start Free Trial</a> 
         {features.map((feature, index) => (
           <li key={index} className={`mb-2 flex items-center mt-8`}>
             <svg
